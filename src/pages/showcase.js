4ed12ea07827e3ecbc4220488a9e53cf893b9d57
mutluhan üzmez <uzmezfurkan@gmail.com>
2019-08-15 11:40:23 +0200

    resimler değişti

--- src/pages/showcase.js
+++ src/pages/showcase.js
@@ -9,6 +9,7 @@ import re4 from '../images/re4.jpg';
 import re5 from '../images/re5.jpg';
 import re6 from '../images/re6.jpg';
 import re7 from '../images/re7.jpg';
+import castle from '../images/castle.jpeg';
 import auvsiSuas2015 from '../images/auvsisuas2015.jpeg';
 import acarrier from '../images/acarrier.jpg';
 import empirestatefurkan from '../images/empirestatefurkan.jpg';
@@ -46,18 +47,14 @@ export default () => (
 
     <table width="512rem">
       <tr>
-        <Subtitle>Antalya</Subtitle>
+       
       </tr>
       <Bordered>
         <td>
-          <Img width="312rem" height="auto" src={re1} alt="Collage of several pictures taken at meetups" />
+          <Img width="312rem" height="auto" src={re1}  />
         </td>
-        <td style={{
-          paddingLeft: 15
-        }}>
-          <Paragraph>
-            With my Friends
-            </Paragraph>
+        <td>
+          <Img width="200rem" height="auto" src={castle}  />
         </td>
       </Bordered>
       <tr>
@@ -65,37 +62,39 @@ export default () => (
 
         </td>
         <td style={{ float: "right" }}>
-          <Subtitle>Maryland,USA</Subtitle>
+          
         </td>
 
       </tr>
+   
       <tr>
-        <td style={{
-          paddingRight: 15
-        }}>
-          <Paragraph>
+        <Subtitle>AUVSI SUAS - 2014 Maryland,USA </Subtitle>
+      </tr>
+      <tr>
+        <td>
+         <Paragraph>
             Award Ceromony
             </Paragraph>
-        </td>
-        <td style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end' }}>
-          <Img width={312} height="auto" src={re2} alt="Collage of several pictures taken at meetups" />
-        </td>
-      </tr>
-      <tr>
-        <Subtitle>AUVSI SUAS - 2014 </Subtitle>
-      </tr>
-      <tr>
-        <td>
+        <Img width={312} height="auto" src={re2} alt="Collage of several pictures taken at meetups" />
         </td>
         <td>
+        <Paragraph>
+           National Air and Space Museum
+            </Paragraph>
           <Img width={312} height="auto" src={re4} alt="Collage of several pictures taken at meetups" />
         </td>
       </tr>
       <tr>
         <td>
+        <Paragraph>
+         Team Presantation 
+            </Paragraph>
           <Img width={312} height="auto" src={re5} alt="Collage of several pictures taken at meetups" />
         </td>
         <td>
+        <Paragraph>
+          MQ-8 Fire Scout
+            </Paragraph>
           <Img width={312} height="auto" src={re6} alt="Collage of several pictures taken at meetups" />
         </td>
       </tr>
